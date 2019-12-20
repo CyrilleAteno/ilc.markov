76e8dee64260089e7a2795e6e5f3273f9c43e869
sg556808 <sg556808@chichester.esirem-ad.ad.u-bourgogne.fr>
2019-12-20 14:13:12 +0100

    commit des commentaires des classes Markov et Couple

--- src/main/java/cfranc/com/Couple.java
+++ src/main/java/cfranc/com/Couple.java
@@ -5,36 +5,42 @@ public class Couple {
 	private String first;
 	private String second;
 	
+        //constructeur de la classe Couple
 	public Couple(String w1, String w2) {
 		init(w1, w2);
 	}
-
+        //initialise les attributs (first et second) avec les Strings donné en paramètre
 	public void init(String w1, String w2) {
 		this.first = w1;
 		this.second = w2;
 	}
-	
+        
+	// permet de recevoir la valeur de l'attribut first
 	public String getFirst() {
 		return first;
 	}
 
+        // permet de définir l'atribut first grâce à un String donné en paramètre 
 	public void setFirst(String first) {
 		this.first = first;
 	}
-
+        // permet de recevoir la valeur de l'attribut second
 	public String getSecond() {
 		return second;
 	}
-
+        // permet de définir l'atribut second grâce à un String donné en paramètre 
 	public void setSecond(String second) {
 		this.second = second;
 	}
-
+        
+        // retourne le hash code du couple
 	@Override
 	public int hashCode() {
 		return hashCodeEclipse();
 	}
-
+        
+        //permet de créé un hash code qui définit un entier qui represente les donées envoyées,
+        //si on réutilise la fct hash code avec les données recu , on peut savoir si la donnée envoyer est la même que celle reçu
 	int hashCodeEclipse() {
 		final int prime = 31;
 		int result = 1;
@@ -43,6 +49,7 @@ public class Couple {
 		return result;
 	}
 	
+        //permet de savoir si les objets on les mêmes attributs et les même valeurs .
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
--- src/main/java/cfranc/com/Markov.java
+++ src/main/java/cfranc/com/Markov.java
@@ -12,10 +12,12 @@ public class Markov {
 
 	HashMap<Couple, List<String>> couples;
 
+        //constructeur de la classe 
 	public Markov() {
 		this.couples = new HashMap<Couple, List<String>>();
 	}
 
+        //cette fct retourne un élément aléatoire dans un collection donnée en paramètre
 	@SuppressWarnings("unused")
 	static <E> E randomElement(Collection<E> c) {
 		int n = (int) (Math.random() * c.size());
@@ -26,12 +28,15 @@ public class Markov {
 			}
 		return null;
 	}
-
+        //retourne un String choisit aléatoirement dans un liste fournit en paramètre
 	private String randomElement(List<String> v) {
 		int n = (int) (Math.random() * v.size());
 		return v.get(n);
 	}
-
+        //lit un fichier et créer des groupe de String tel que :
+        // mot1 et mot2 avec mot 3 , mot2 et mot 3 avec mot 4 jusqu'a ce que le fichier soit lu en entier 
+        //ensuite les couples de mots sont rentrés dans une liste 
+        // la fct permet de détecter si un mot éxiste déja  , si ce n'est pas le cas alors elle l'ajoute a la liste
 	public void readFile(String path) throws FileNotFoundException{
 		Scanner sc;
 		sc = new Scanner(new File(path));
@@ -59,7 +64,10 @@ public class Markov {
 		}
 		sc.close();
 	}
-
+        
+        // permet de générer un nombre de mots aléatoire apres avoir fournit un couple de mot , la fct retourne une chaine de caratère avec le nombre de mot voulu
+        // tant que le nb de mot a avoir n'est pas atteint la fct prends le couple de mot et un mot aléatoire qui est en référence avec le couple choisit
+        // si il n'y a pas de mot en rapport avec le couple de mot la boucle s'arrête
 	public String generateText(Couple p, int words) {
 		String res = p.getFirst() + " " + p.getSecond() + " ";
 	    words -= 2;
