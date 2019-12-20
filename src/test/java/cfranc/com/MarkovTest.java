86fc21662cdc5ba7c58d78dff621a87db97b7b7f
mekongoabanda <mekongoabanda21@gmail.com>
2019-12-20 10:55:38 +0100

    commit README & TEST couple

--- src/test/java/cfranc/com/MarkovTest.java
+++ src/test/java/cfranc/com/MarkovTest.java
@@ -4,7 +4,18 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import java.util.HashMap;
+import java.util.List;
+
 public class MarkovTest {
+
+
+	private String first = " i'm first";
+	private String second = " i'm second";
+	private Markov mMarkov = new Markov();
+
+
+	// TODO ------------------------- LE FICHIER "COSETTE.TXT" n'arrive pas à être détecté ----------------------------------------
 
 	@Test
 	public void test() {
@@ -14,6 +25,15 @@ public class MarkovTest {
 		String res = m.generateText(p, 10);
 		assertNotNull(res);
 		System.out.println(res);
+	}
+
+	@Test
+	public void GenerateText(){
+
+		HashMap<Couple, List<String>> couples;
+		
+
+
 	}
 
 }
