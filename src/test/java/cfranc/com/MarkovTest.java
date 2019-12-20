e060f5f1a7bed020b767df8bb05ce258b0e2fef7
dm663891 <dm663891@chichester.esirem-ad.ad.u-bourgogne.fr>
2019-12-20 10:27:40 +0100

    Ajout de fichiers test pour tester la fonction readFile de Markov

--- src/test/java/cfranc/com/MarkovTest.java
+++ src/test/java/cfranc/com/MarkovTest.java
@@ -5,7 +5,12 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class MarkovTest {
-
+    
+    Couple c1 = new Couple("Bonjour","Bonsoir");
+    Couple c2 = new Couple("Bonjour", "Bonsoir");
+    Couple c3 = new Couple("Au revoir", "Bonnes vacances");
+    int w = 10;
+    
 	@Test
 	public void test() {
             // Exemple d'utilisation du programme
@@ -19,5 +24,20 @@ public class MarkovTest {
 		assertNotNull(res);
 		System.out.println(res);
 	}
+        
+        @Test
+        public void readFile()
+        {
+            // si il y a un mot on stock ce mot et on fait un deuxieme if
+            // si il y a pas de mot apres le premier mot on close
+            // si il y a un mot on cree un couple
+            // si il y a un troisieme, on entre dans le while
+            // si il 'y a jamais eu de mots associe a ce couple la 
+            // on rajoute le mot comme compatible, on ajoute le troisieme comme mot qui suit les deux premiers
+            // hashmap ou on cherche tout les troiième mot qui peuvent aller avec un couple de mot
+            
+            // 4 fichiers pour tester
+        }
+        
 
 }
