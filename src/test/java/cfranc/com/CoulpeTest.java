5b8d04a86137a1f1f62fdaa7f4b797a105c03c1a
mekongoabanda <mekongoabanda21@gmail.com>
2019-12-20 10:37:57 +0100

    commit README & TEST couple

--- src/test/java/cfranc/com/CoulpeTest.java
+++ src/test/java/cfranc/com/CoulpeTest.java
@@ -6,19 +6,47 @@ import org.junit.Test;
 
 public class CoulpeTest {
 
-	private String first = "je suis premier";
-	private String second = "je suis 2e";
+	private String first = null;
+	private String second = null;
+	private Couple mCouple = new Couple( first, second);
 
 
-	//Tester le retour du premier element;
+
+	//les deux éléments sont null
 	@Test
-	public void testGetFirst() {
-		System.out.println("Not yet implemented");
+	public void hashCodeEclipseTest_All_NULL(){
+
+		int expected = 31 * 31;
+		int actual = mCouple.hashCodeEclipse();
+		assertEquals(expected, actual);
+
 	}
 
+	//Aucun élément n'est null
 	@Test
-	public void hashCodeEclipseTest(){
+	public void hashCodeEclipseTest_Return_All_Not_NULL(){
 
+		mCouple.setFirst("Je suis first");
+		mCouple.setSecond("Je suis second");
+		int expected = 31 + mCouple.getFirst().hashCode();
+		expected = expected * 31 + mCouple.getSecond().hashCode();
+
+		int actual = mCouple.hashCodeEclipse();
+		assertEquals(expected, actual);
+
+	}
+
+	//Un seul élément est null
+	@Test
+	public void hashCodeEclipseTest_Return_One_Null(){
+
+		//result = prime * result + ((first == null) ? 0 : first.hashCode());
+		//result = prime * result + ((second == null) ? 0 : second.hashCode());
+		mCouple.setFirst("Je suis first");
+		int expected = 31 + mCouple.getFirst().hashCode();
+		expected = expected * 31;
+		int actual = mCouple.hashCodeEclipse();
+		assertEquals(expected, actual);
 
 	}
 
