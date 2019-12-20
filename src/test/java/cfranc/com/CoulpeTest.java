ddbcc780cc6e3c28d19782994210b80cc9dc6948
Florian <50233689+flofiz@users.noreply.github.com>
2019-12-20 15:16:04 +0100

    test des classes couple et markov

--- src/test/java/cfranc/com/CoulpeTest.java
+++ src/test/java/cfranc/com/CoulpeTest.java
@@ -5,10 +5,54 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class CoulpeTest {
-
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+        
+        @Test
+        public void equals_hasTheSameAddress()
+        {
+            Couple c1 = new Couple("first","second");
+            Couple c2 = c1;
+            boolean expected = true;
+            boolean actual = c1.equals(c2);
+            assertEquals(expected, actual);
+        }
+        
+        @Test
+        public void equals_isNotEqualsToNull()
+        {
+            Couple c1 = new Couple("first","second");
+            Couple c2 = null;
+            boolean expected = false;
+            boolean actual = c1.equals(c2);
+            assertEquals(expected, actual);
+        }
+        
+        @Test
+        public void equals_isNotEqualsToInstanceOfAnotherClass()
+        {
+            Couple c1 = new Couple("first","second");
+            int c2 = 12;
+            boolean expected = false;
+            boolean actual = c1.equals(c2);
+            assertEquals(expected, actual);
+        }
+        
+        @Test
+        public void equals_firstEqualsNullButNotTheOther()
+        {
+            Couple c1 = new Couple(null,"second");
+            Couple c2 = new Couple("first","second");
+            boolean expected = false;
+            boolean actual = c1.equals(c2);
+            assertEquals(expected, actual);
+        }
+        
+        public void equals_firstDiferentToOtherFirst()
+        {
+            Couple c1 = new Couple("first","second");
+            Couple c2 = new Couple("other first","second");
+            boolean expected = false;
+            boolean actual = c1.equals(c2);
+            assertEquals(expected, actual);
+        }
 
 }
