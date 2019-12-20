5fa1ceec4821f2f62c631f69abba960121344ed7
dm663891 <dm663891@chichester.esirem-ad.ad.u-bourgogne.fr>
2019-12-20 09:17:51 +0100

    Insertion de commentaire sur la classe MarkovTest

--- src/test/java/cfranc/com/MarkovTest.java
+++ src/test/java/cfranc/com/MarkovTest.java
@@ -8,9 +8,13 @@ public class MarkovTest {
 
 	@Test
 	public void test() {
+            // Exemple d'utilisation du programme
 		Markov m = new Markov();
+                // fichier qui lui apprend a parler comme quelqu'un
 		m.readFile("cosette.txt");
+                // va chercher dans l'objet Markov un debut de phrase aleatoire
 		Couple p = Markov.randomElement(m.couples.keySet());
+                // On prend le debut de phrase aleatoire et on cree une phrase aleatoire de 10 mots a partir de ca
 		String res = m.generateText(p, 10);
 		assertNotNull(res);
 		System.out.println(res);
