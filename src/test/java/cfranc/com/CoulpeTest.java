58d8803b3c99b31a6e114e4f23eddee0dafe5df1
jm522783 <jm522783@chichester.esirem-ad.ad.u-bourgogne.fr>
2019-12-20 15:21:24 +0100

    Ajout des tests

--- src/test/java/cfranc/com/CoulpeTest.java
+++ src/test/java/cfranc/com/CoulpeTest.java
@@ -7,8 +7,14 @@ import org.junit.Test;
 public class CoulpeTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void equals_StringObject_False() {
+		Couple c = new Couple("hi","hj");
+                assertEquals(c.equals("f"),false);
 	}
-
+        
+        @Test
+        public void equals_CoupleObject_True() {
+		Couple c = new Couple("hi","hj");
+                assertEquals(c.equals(c),true);
+	}
 }
