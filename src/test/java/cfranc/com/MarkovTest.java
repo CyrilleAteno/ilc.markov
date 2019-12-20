785efefb2278d31fc877573a1223cba882d0ef29
wa756722 <wa756722@chichester.esirem-ad.ad.u-bourgogne.fr>
2019-12-20 10:58:56 +0100

    les fonctions a tester

--- src/test/java/cfranc/com/MarkovTest.java
+++ src/test/java/cfranc/com/MarkovTest.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class MarkovTest {
-
+        
 	@Test
 	public void test() {
 		Markov m = new Markov();
@@ -15,5 +15,46 @@ public class MarkovTest {
 		assertNotNull(res);
 		System.out.println(res);
 	}
+
+        
+        @Test
+        public void readFiletest4(){
+                Markov m = new Markov();
+                m.readFile("test4.txt");
+		Couple p = Markov.randomElement(m.couples.keySet());
+		String res = m.generateText(p, 10);
+		assertNotNull(res);
+		System.out.println(res);
+        
+        
+        }
+        
+        @Test
+        public void readFiletest1(){
+            Markov m = new Markov();
+            m.readFile("test1.txt");
+            assertEquals(0, m.couples.size());
+        
+        }
+        
+           @Test
+        public void readFiletest2(){
+            Markov m = new Markov();
+            m.readFile("test2.txt");
+            assertEquals(0, m.couples.size());
+        
+        }
+        
+           @Test
+        public void readFiletest3(){
+            Markov m = new Markov();
+            m.readFile("test3.txt");
+            assertEquals(1, m.couples.size());
+        
+        }
+        
+       
+        
+
 
 }
