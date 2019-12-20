436b99f46f704b906d5f7353cb2b9fc46b4fd2ce
TOURE Sidi <siditouredjenne@gmail.com>
2019-12-20 11:03:45 +0100

    apres implementation des tests

--- src/test/java/cfranc/com/CoulpeTest.java
+++ src/test/java/cfranc/com/CoulpeTest.java
@@ -8,7 +8,15 @@ public class CoulpeTest {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		//fail("Not yet implemented");
+		// ici on teste seulement la fonction hashCode
+		Couple aObject = new Couple("alors", "comment");
+		Couple bObject = new Couple("alors", "comment");
+		Couple cObject = new Couple("redire", "chose");
+
+		assertTrue(aObject.hashCode() == bObject.hashCode()); //true car les deux objets sont les meme
+		assertFalse(aObject.hashCode() == cObject.hashCode()); //doit etre false car les deux objets sont differents
+
 	}
 
 }
--- src/test/java/cfranc/com/MarkovTest.java
+++ src/test/java/cfranc/com/MarkovTest.java
@@ -16,4 +16,31 @@ public class MarkovTest {
 		System.out.println(res);
 	}
 
+	@Test
+	public  void test1(){
+		//a voir
+	}
+	@Test
+	public  void test2(){
+		//a voir
+	}
+	@Test
+	public  void test3(){
+		Markov m = new Markov();
+		m.readFile("test3.txt");
+		Couple p = Markov.randomElement(m.couples.keySet());
+		String res = m.generateText(p, 10);
+		assertNotNull(res);
+		System.out.println(res);
+	}
+	@Test
+	public  void test4(){
+		Markov m = new Markov();
+		m.readFile("test4.txt");
+		Couple p = Markov.randomElement(m.couples.keySet());
+		String res = m.generateText(p, 10);
+		assertNotNull(res);
+		System.out.println(res);
+	}
+
 }
