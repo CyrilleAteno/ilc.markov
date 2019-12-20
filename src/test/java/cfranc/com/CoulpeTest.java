ccdbb5b938ed59c95e6061948b7a405dfcf9794a
wa756722 <wa756722@chichester.esirem-ad.ad.u-bourgogne.fr>
2019-12-20 10:58:44 +0100

    les fonctions a tester

--- src/test/java/cfranc/com/CoulpeTest.java
+++ src/test/java/cfranc/com/CoulpeTest.java
@@ -5,10 +5,31 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class CoulpeTest {
-
+        Couple cp = new Couple("Hello", "World");
+        Couple cp1 = new Couple("It's", "Me");
+        
 	@Test
 	public void test() {
 		//fail("Not yet implemented");
+                Couple cp = new Couple("Hello", "World");
+                System.out.println(cp.hashCode());
+                        
 	}
+        
+        @Test
+        public void testHashCode() {
+            int expected = 153375780;
+            assertNotEquals(expected, cp.hashCodeEclipse());
+        
+        }
+        
+        @Test
+        public void nullEquals(){
+            
+            boolean res = cp.equals(null);
+            assertEquals(false, res);
+        }
+        
+        
 
 }
