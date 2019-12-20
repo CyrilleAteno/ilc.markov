0d645f0cb556c0a6517430a9794d1b25f5cbcac1
jm512325 <jm512325@chichester.esirem-ad.ad.u-bourgogne.fr>
2019-12-20 09:53:04 +0100

    Tests de Markov

--- src/test/java/cfranc/com/MarkovTest.java
+++ src/test/java/cfranc/com/MarkovTest.java
@@ -15,5 +15,29 @@ public class MarkovTest {
 		assertNotNull(res);
 		System.out.println(res);
 	}
-
+        
+        @Test
+	public void creation__NotNull() {
+		Markov m = new Markov();
+                assertNotNull(m);
+	}
+        
+        @Test
+	public void readFile_FileNotNull_couplesNotEmpty() {
+		Markov m = new Markov();
+		m.readFile("cosette.txt");
+		
+                assertNotEquals(0, m.couples.size());
+	}
+        
+        
+        // ----- Fonction generateText -----
+        @Test
+	public void generateText_couple0_TRUE() {
+            Markov m = new Markov();
+            Couple couple = new Couple("un","couple");
+            String expected = couple.getFirst() + " " + couple.getSecond() + " ";
+            
+            assertEquals(expected, m.generateText(couple, 0));
+	}
 }
